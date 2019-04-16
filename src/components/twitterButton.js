61ff1843d76d3119cf2834820fb3e9f773dd03be
Alexandros Spyropoulos <spyropoulos.a@gmail.com>
2019-04-16 02:46:47 +0100

    Adds TwitterButton

---
+++ src/components/twitterButton.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const TwitterButton = () => (
+    <a 
+      href="https://twitter.com/RPGCommunityLDN?ref_src=twsrc%5Etfw"
+      className="twitter-follow-button"
+      data-show-count="false">Follow @RPGCommunityLDN</a>
+)
+
+export default TwitterButton
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,12 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import TwitterButton from '../components/twitterButton';
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`london`, 'rpg', 'community', 'games', 'tabletop', 'role', 'playing', 'games']} />
-    <h1>Under Construction</h1>
+    <TwitterButton/> 
   </Layout>
 )
 
