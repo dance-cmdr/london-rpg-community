5b2e8e22e54baa814b823e79bce22a665b67a280
Alexandros Spyropoulos <spyropoulos.a@gmail.com>
2019-04-16 02:48:15 +0100

    Adds Twitter Feed

---
+++ src/components/twitterFeed.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+const TwitterFeed = () => (
+    <section>
+      <h2>Our twitter activity</h2>
+      <a
+        className="twitter-timeline"
+        href="https://twitter.com/RPGCommunityLDN?ref_src=twsrc%5Etfw">Tweets by RPGCommunityLDN
+      </a>
+    </section>
+)
+
+export default TwitterFeed
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,14 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import TwitterButton from '../components/twitterButton';
+import TwitterButton from '../components/twitterButton'
+import TwitterFeed from '../components/twitterFeed'
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`london`, 'rpg', 'community', 'games', 'tabletop', 'role', 'playing', 'games']} />
     <TwitterButton/> 
+    <TwitterFeed/>
   </Layout>
 )
 
