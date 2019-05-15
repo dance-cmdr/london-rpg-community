1d6a4805371b3296b3852a6872b40b266f8edaac
Alexandros Spyropoulos <1997886+dance-cmdr@users.noreply.github.com>
2019-05-15 23:58:45 +0100

    Favicon
    * adds favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,7 +35,7 @@ module.exports = {
         background_color: PRIMARY_COLOR,
         theme_color: PRIMARY_COLOR,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/lrpgc-favicon.png`,
       },
     },
     `gatsby-plugin-twitter`,
